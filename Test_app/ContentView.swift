5b25c9e4f861336055f52c3c5eef9a4a2a00c2e1
Oh-JongJin <5jx2oh@gmail.com>
2023-01-03 20:06:23 +0900

    Initial Commit

---
+++ Test_app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Test_app
+//
+//  Created by 오종진 on 2023/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test_app/Test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_appApp.swift
+//  Test_app
+//
+//  Created by 오종진 on 2023/01/03.
+//
+
+import SwiftUI
+
+@main
+struct Test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
